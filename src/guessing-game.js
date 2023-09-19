7c2403acaf4d804cf8831fe420adb26743bddb91
Leyla <neyseltheblack@gmail.com>
2023-09-19 21:04:23 +0500

    Finally

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+            this.number = null
+            this.min = null 
+            this.max = null
+            this.arr = null
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
+        this.number = this.max - Math.floor((this.max - this.min) / 2)
+console.log('max='+this.max)
+console.log('mmin='+this.min)
 
+        console.log(this.number)
+        // console.log(Math.floor((this.max - this.min) / 2))
+        // console.log(this.max / 2)
+        // console.log((this.min / 2) + this.min)
+
+        console.log('rand='+ Math.floor(this.min + Math.random() * (this.max  + 1 - this.min)))
+        return this.number
     }
 
     lower() {
-
+        this.max = this.number 
     }
 
     greater() {
-
+        this.min = this.number 
     }
 }
 
